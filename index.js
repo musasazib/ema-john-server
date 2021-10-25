69e19bf40cf02f546f9fdc7d08b8b5ba67f4b4e8
musasazib <musasazib@gmail.com>
2021-10-26 01:26:42 +0600

    Add express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Ema John server is running');
+});
+
+app.listen(port, () => {
+    console.log('Server running at port', port);
+});
