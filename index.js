399f18e1178be7bb1e6aef82adc369e6b83e26b7
musasazib <musasazib@gmail.com>
2021-10-26 03:33:22 +0600

    Add data from JSON to database and load on the UI

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyv3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('onlineShop');
+        const productCollection = database.collection('products');
+
+        // Get API 
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Ema John server is running');
