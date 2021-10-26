95815cc636aa990f703c515386e8b043e2a36dbe
musasazib <musasazib@gmail.com>
2021-10-26 17:05:12 +0600

    Add shipping

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
         await client.connect();
         const database = client.db('onlineShop');
         const productCollection = database.collection('products');
+        const orderCollection = database.collection('orders');
 
         // Get API 
         app.get('/products', async (req, res) => {
@@ -40,6 +41,22 @@ async function run() {
                 products
             });
         })
+
+        // Use POST to get data by keys
+        app.post('/products/byKeys', async (req, res) => {
+            const keys = req.body;
+            const query = { key: { $in: keys } }
+            const products = await productCollection.find(query).toArray();
+            res.json(products);
+        });
+
+        // Add order POST API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+
     }
     finally {
         // await client.close();
